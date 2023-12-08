8ca4b16f3ecda0216a9d426b8f026ccf6aeb9952
Hritikgadgil0361 <85680735+Hritikgadgil0361@users.noreply.github.com>
2023-12-08 11:35:52 +0000

    Arrays Basics

---
+++ .devcontainer/01_basics/Array.js
@@ -0,0 +1,14 @@
+ const myArray = [0,1,2,3,4];
+ const myHeros = ["Batman", "hitman","Spiderman"]
+ const myArray2 = new Array(1,2,3,4)
+ 
+ myHeros.push("Captain")   // Adds the value to the array
+ //console.log(myArray2[0])
+ //console.log(myHeros)
+ myHeros.pop()               // Removes last value
+ myHeros.unshift("Virat")
+ myHeros.shift()
+ //console.log(myHeros);
+ //console.log(myHeros.join())
+
+ //********************Slice and Splice */
