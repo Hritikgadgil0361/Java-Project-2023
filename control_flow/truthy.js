2cd5bb8d3a689f3b58bd2c42cf68b1322ec878cd
Hritikgadgil0361 <85680735+Hritikgadgil0361@users.noreply.github.com>
2023-12-19 10:03:48 +0000

    Truthy and falty values

---
+++ control_flow/truthy.js
@@ -0,0 +1,14 @@
+// Nullish coalescing operator ?? : null undefined 
+
+/*let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+val1 = null ?? undefined
+console.log(val1);*/
+
+//Terniary opertor ?
+//condition ? true : false  checks either condition is true or false without if else condition
+const iceprice = 100
+iceprice >= 80 ? console.log("less than 80") : console.log("Greater than 80");
