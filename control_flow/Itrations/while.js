46be9cb67bba2b193893298b125b485cc1bffc2b
Hritikgadgil0361 <85680735+Hritikgadgil0361@users.noreply.github.com>
2023-12-19 13:34:23 +0000

    While loop

---
+++ control_flow/Itrations/while.js
@@ -0,0 +1,13 @@
+/*let myarray = [red, blue, green]
+let arr = 0
+while ( arr < myarray.length) {
+    console.log('value is ${myarray[arr]}');
+    arr  = arr++
+    
+}*/
+
+let score = 1
+do{
+    console.log(score);
+    score++
+}while (score <= 10)
